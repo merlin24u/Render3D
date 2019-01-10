eba85c427abfd6f9cea7f6b2de2ef76a01462c54
Paul Merlin <paul.merlin1@etu.univ-lorraine.fr>
2019-01-10 19:20:42 +0100

    draw line

--- main.cpp
+++ main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
+#include "tgaimage.h"
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red   = TGAColor(255, 0,   0,   255);
+
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+    bool steep = false;
+    if (abs(x0-x1)<abs(y0-y1)) { // if the line is steep, we transpose the image
+        swap(x0, y0);
+        swap(x1, y1);
+        steep = true;
+    }
+    if (x0>x1) { // make it left−to−right
+        swap(x0, x1);
+        swap(y0, y1);
+    }
+    for (int x=x0; x<=x1; x++) {
+        float t = (x-x0)/(float)(x1-x0);
+        int y = y0*(1.-t) + y1*t;
+        if (steep) {
+            image.set(y, x, color); // if transposed, de−transpose
+        } else {
+            image.set(x, y, color);
+        }
+    }
+}
+
+
+int main() {
+    TGAImage image(100, 100, TGAImage::RGB);
+    line(13, 20, 80, 40, image, white);
+    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    image.write_tga_file("image.tga");
     return 0;
 }
