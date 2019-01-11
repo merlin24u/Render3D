6c80917e285b13ab1834c476e2d3db9d1590f7e1
Paul Merlin <paul.merlin1@etu.univ-lorraine.fr>
2019-01-11 17:56:14 +0100

    coloration un triangle

--- geometry.h
+++ geometry.h
@@ -1,9 +1,35 @@
 #ifndef GEOMETRY_H
 #define GEOMETRY_H
 
-class Vect3{
+class Vect2{
 public :
-    float tab[3];
+    float tab[2];
+    Vect2(){}
+    Vect2(int x,int y){
+        tab[0] = x;
+        tab[1] = y;
+    }
 };
+
+Vect2 operator+(Vect2 const& v,Vect2 const& v2){
+    Vect2 res;
+    res.tab[0] = v.tab[0] + v2.tab[0];
+    res.tab[1] = v.tab[1] + v2.tab[1];
+    return res;
+}
+
+Vect2 operator-(Vect2 const& v,Vect2 const& v2){
+    Vect2 res;
+    res.tab[0] = v.tab[0] - v2.tab[0];
+    res.tab[1] = v.tab[1] - v2.tab[1];
+    return res;
+}
+
+Vect2 operator*(Vect2 const& v,float f){
+    Vect2 res;
+    res.tab[0] = f*v.tab[0];
+    res.tab[1] = f*v.tab[1];
+    return res;
+}
 
 #endif // GEOMETRY_H
--- main.cpp
+++ main.cpp
@@ -27,35 +27,62 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     for (int x=x0; x<=x1; x++) {
         float t = (x-x0)/(float)(x1-x0);
         int y = y0*(1.-t) + y1*t;
-        if (steep) {
+        if (steep)
             image.set(y, x, color); // if transposed, de−transpose
-        } else {
+        else
             image.set(x, y, color);
+    }
+}
+
+void triangle(Vect2 v1,Vect2 v2,Vect2 v3,TGAImage &image, TGAColor color){
+    if(v1.tab[1]==v2.tab[1] && v1.tab[1]==v3.tab[1]) return;
+    if(v1.tab[1]>v2.tab[1])
+        swap(v1,v2);
+    if(v1.tab[1]>v3.tab[1])
+        swap(v1,v3);
+    if(v2.tab[1]>v3.tab[1])
+        swap(v2,v3);
+    int total_h = v3.tab[1]-v1.tab[1];
+    for(int i=0;i<total_h;i++){
+        bool second_half = i>v2.tab[1]-v1.tab[1] || v2.tab[1]==v1.tab[1];
+        int segment_height = second_half ? v3.tab[1]-v2.tab[1] : v2.tab[1]-v1.tab[1];
+        float alpha = (float)i/total_h;
+        float beta  = (float)(i-(second_half ? v2.tab[1]-v1.tab[1] : 0))/segment_height;
+        Vect2 A = v1 + (v3-v1)*alpha;
+        Vect2 B = second_half ? v2 + (v3-v2)*beta : v1 + (v2-v1)*beta;
+        if (A.tab[0]>B.tab[0])
+            swap(A, B);
+        for (int j=A.tab[0]; j<=B.tab[0]; j++) {
+            image.set(j, v1.tab[1]+i, color); // attention, due to int casts t0.y+i != A.y
         }
     }
 }
 
-int main() {
-    ifstream file;
-    file.open ("obj/african_head.obj", ifstream::in);
+int main(int argc, char** argv) {
+    /*ifstream file;
+    if(argc==2)
+        file.open (argv[1], ifstream::in);
+    else
+        file.open ("obj/african_head.obj", ifstream::in);
+
     if (file.fail()) {
         cout << "Erreur lors de l'ouverture du fichier" << endl;
         return 0 ;
     }
 
-    vector<Vect3> points, faces;
-    string ligne;
+    vector<Vect2> points, faces;
+    string l;
     while(!file.eof()){
-        getline(file, ligne);
-        istringstream stream(ligne.c_str());
+        getline(file, l);
+        istringstream stream(l.c_str());
         char t;
-        if (ligne.compare(0, 2, "v ") == 0) {
+        if (l.compare(0, 2, "v ") == 0) {
             stream >> t;
             Vect3 v;
-            for(int i=0;i<3;i++)
+            for(int i=0;i<2;i++)
                 stream >> v.tab[i];
             points.push_back(v);
-        }else if(ligne.compare(0, 2, "f ") == 0){
+        }else if(l.compare(0, 2, "f ") == 0){
             int index,t2;
             Vect3 v;
             stream >> t;
@@ -69,10 +96,10 @@ int main() {
     }
 
     TGAImage image(width, height, TGAImage::RGB);
-    for(vector<Vect3>::iterator it = faces.begin(); it != faces.end(); ++it) {
+    for(vector<Vect2>::iterator it = faces.begin(); it != faces.end(); ++it) {
         for(int i=0;i<3;i++){
-            Vect3 v0 = points[it->tab[i]];
-            Vect3 v1 = points[it->tab[(i+1)%3]];
+            Vect2 v0 = points[it->tab[i]];
+            Vect2 v1 = points[it->tab[(i+1)%3]];
             int x0 = (v0.tab[0]+1.0)*width/2;
             int y0 = (v0.tab[1]+1.0)*width/2;
             int x1 = (v1.tab[0]+1.0)*width/2;
@@ -80,7 +107,13 @@ int main() {
             line(x0, y0, x1, y1, image, white);
         }
     }
+    */
 
+    TGAImage image(width, height, TGAImage::RGB);
+    Vect2 v1(10,70);
+    Vect2 v2(50,160);
+    Vect2 v3(70,80);
+    triangle(v1,v2,v3,image,red);
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("image.tga");
     return 0;
